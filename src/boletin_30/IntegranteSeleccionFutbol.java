a44aafa961ec82bdf9c64b8871ea095bfa16add6
Marcos <Marcos@casa-equipo2.mundo-R.com>
2018-06-10 17:25:00 +0200

    30

---
+++ src/boletin_30/IntegranteSeleccionFutbol.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_30;
+
+/**
+ *
+ * @author Marcos
+ */
+public interface IntegranteSeleccionFutbol {
+
+    public void concentrarse();
+    public void viajar();
+    public void entrenar();
+    public void jugarPartido();
+}
